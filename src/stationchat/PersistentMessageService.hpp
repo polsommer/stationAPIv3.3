ce4546445489d853f17b2d751fa0d0b94e241555
polsommer <patricker@live.dk>
2026-02-27 10:51:29 +0100

    uptodate

---
+++ src/stationchat/PersistentMessageService.hpp
@@ -0,0 +1,33 @@
+
+#pragma once
+
+#include "ChatEnums.hpp"
+#include "PersistentMessage.hpp"
+
+#include <boost/optional.hpp>
+
+#include <cstdint>
+#include <vector>
+
+struct sqlite3;
+
+class PersistentMessageService {
+public:
+    explicit PersistentMessageService(sqlite3* db);
+    ~PersistentMessageService();
+
+    void StoreMessage(PersistentMessage& message);
+
+    std::vector<PersistentHeader> GetMessageHeaders(uint32_t avatarId);
+
+    PersistentMessage GetPersistentMessage(uint32_t avatarId, uint32_t messageId);
+
+    void UpdateMessageStatus(
+        uint32_t avatarId, uint32_t messageId, PersistentState status);
+
+    void BulkUpdateMessageStatus(
+        uint32_t avatarId, const std::u16string& category, PersistentState newStatus);
+
+private:
+    sqlite3* db_;
+};
